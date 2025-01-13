631d8648181558dbabcf09fd880719a2077fb14d
Raf <rafaelgutierrez07@icloud.com>
2025-01-12 22:55:24 -0800

    added ui to main menu

--- Passion Project 2/Assets/Scripts/Debugging.cs
+++ Passion Project 2/Assets/Scripts/Debugging.cs
@@ -2,9 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class Debugging : MonoBehaviour
 {
+    [SerializeField] private bool showFPS;
     [SerializeField] private TextMeshProUGUI fpsText;
     [Tooltip("How often it counts the frames."), Min(0), SerializeField] private float pollingTime = 1;
 
@@ -13,6 +15,8 @@ public class Debugging : MonoBehaviour
 
     private void Update()
     {
+        fpsText.enabled = showFPS;
+
         time += Time.deltaTime;
 
         frameCount++;
@@ -25,5 +29,10 @@ public class Debugging : MonoBehaviour
             time -= pollingTime;
             frameCount = 0;
         }
+    }
+
+    public void ShowFPS(Toggle toggle)
+    {
+        showFPS = toggle.isOn;
     }
 }
